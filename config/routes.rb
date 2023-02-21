09ecd306d56bdbefb0b3093d7af28e9590ff2e01
James Corfe <james.corfe@gmail.com>
2023-02-21 16:44:46 +0000

    delete function added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get "tasks", to: "tasks#index"
 
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new_task
 
   post "tasks", to: "tasks#create"
 
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get "tasks/:id", to: "tasks#show", as: :task
 
-  get "tasks/:id", to: "tasks#show", as: :delete_task
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
