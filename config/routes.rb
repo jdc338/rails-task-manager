525edb31a242fea223ac87a214ae573b2a2b23e9
James Corfe <james.corfe@gmail.com>
2023-02-21 16:18:29 +0000

    edit funtion added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 
   get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new"
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
   get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks/:id", to: "tasks#show", as: :delete_task
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
